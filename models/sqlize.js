7eb4b9613acdab4e4ea3b5f80d5107b907af8f18
Alan Van Art <alanv73@gmail.com>
2020-04-07 17:05:59 -0400

    add wind spd/gust gauge add cpu temp data model remove plotly gauge

--- models/sqlize.js
+++ models/sqlize.js
@@ -1,24 +1,10 @@
 const { Sequelize, Model, DataTypes } = require('sequelize');
 
 // test server
-// const sequelize = new Sequelize('weather', 'alan', 'qnapn3sov', {
-//     host: 'athenaeum',
-//     port: 3306,
-//     dialect: 'mysql',
-//     dialectOptions: {
-//         timezone: process.env.db_timezone
-//     },
-//     logging: false,
-//     define: {
-//         freezeTableName: true
-//     }
-// });
-
-// RPi
-const sequelize = new Sequelize('weather', 'pi', 'mysqln3sov', {
-    host: 'localhost',
+const sequelize = new Sequelize('weather', 'alan', 'qnapn3sov', {
+    host: 'athenaeum',
     port: 3306,
-    dialect: 'mariadb',
+    dialect: 'mysql',
     dialectOptions: {
         timezone: process.env.db_timezone
     },
@@ -27,5 +13,19 @@ const sequelize = new Sequelize('weather', 'pi', 'mysqln3sov', {
         freezeTableName: true
     }
 });
+
+// RPi
+// const sequelize = new Sequelize('weather', 'pi', 'mysqln3sov', {
+//     host: 'localhost',
+//     port: 3306,
+//     dialect: 'mariadb',
+//     dialectOptions: {
+//         timezone: process.env.db_timezone
+//     },
+//     logging: false,
+//     define: {
+//         freezeTableName: true
+//     }
+// });
 
 module.exports = sequelize;
