37976de4f12787b5cac53f87ecf50ab60f0499e1
Alan Van Art <alanv73@gmail.com>
2020-04-22 15:55:59 -0400

    Merge branch 'master' of https://github.com/alanv73/RPi_Weather_Station_Interface

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,63 @@ const { Sequelize, Model, DataTypes } = require('sequelize');
 const express = require('express'),
     app = express(),
     router = express.Router(),
+<<<<<<< HEAD:routes/index.js
     sequelize = require('../models/sqlize'),
     WxMeasurement = require('../models/wxmeasr');
+=======
+    sequelize = require('./models/sqlize'),
+    WxMeasurement = require('./models/wxmeasr');
+const port = process.env.PORT;
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+async function testDB() {
+    await sequelize.authenticate().then(() => {
+        console.log('Connection has been established successfully.');
+        closeDB();
+    }).catch(err => {
+        console.error('Unable to connect to the database:\n', err.message);
+    });
+}
+
+async function selectAll() {
+    let wxData;
+    const allData = await WxMeasurement.findAll({
+        attributes: [
+            'AMBIENT_TEMPERATURE', 'GROUND_TEMPERATURE', 'AIR_PRESSURE',
+            'HUMIDITY', 'WIND_DIRECTION', 'WIND_SPEED',
+            'WIND_GUST_SPEED', 'WIND_CHILL', 'HEAT_IDX', 'DEW_PT',
+            'RAINFALL', 'CREATED'
+        ]
+    }).then((data) => {
+        console.log('found.');
+        // let wxData = JSON.stringify(data, null, 2)
+        data.forEach(dataPoint => {
+            console.log(
+                dataPoint.CREATED,
+                dataPoint.AMBIENT_TEMPERATURE,
+                dataPoint.GROUND_TEMPERATURE,
+                dataPoint.AIR_PRESSURE,
+                dataPoint.HUMIDITY,
+                dataPoint.WIND_DIRECTION
+            );
+        });
+        closeDB();
+    }).catch(err => {
+        console.error('Error :\n', err.message);
+    });
+    return wxData;
+}
+
+function closeDB() {
+    sequelize.close().then(() => {
+        console.log('Connection successfully closed.');
+    }).catch(err => {
+        console.log(`Error: ${err}`);
+    });
+}
+>>>>>>> 3230c5146296bf429b4f876f7e2425476f02eb54:index.js
 
 // INDEX Route - show current conditions
 router.get('/', (req, res) => {
@@ -70,4 +125,14 @@ router.get('/', (req, res) => {
 
 });
 
+<<<<<<< HEAD:routes/index.js
 module.exports = router;
+=======
+/*
+ * Listen for connections on port 3000
+ */
+app.listen(port || 3000, () => {
+    console.log(`WxPi listening on port ${port || 3000}...`);
+});
+
+>>>>>>> 3230c5146296bf429b4f876f7e2425476f02eb54:index.js
