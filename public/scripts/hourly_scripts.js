9637683f114d8b3baff68dbd47acb94e6b13877f
alanv73 <alanv73@gmail.com>
2020-04-23 20:36:44 -0400

    add two more charts to hourly, 2 series per chart

--- public/scripts/hourly_scripts.js
+++ public/scripts/hourly_scripts.js
@@ -1,8 +1,8 @@
-// build the hourly rain chart
+// build the hourly ground/air temp chart
 function chartTemp() {
-    const x_data = arguments[0];
-    const ctx = document.getElementById('temp').getContext('2d');
-    const myChart = new Chart(ctx, {
+    const cat_data = arguments[0];
+    const ctz = document.getElementById('temp').getContext('2d');
+    const tempChart = new Chart(ctz, {
         type: 'line',
         defaults: {
             global: {
@@ -10,7 +10,7 @@ function chartTemp() {
             }
         },
         data: {
-            labels: x_data,
+            labels: cat_data,
             datasets: [{
                 label: 'Ambient Temperature °F',
                 yAxisID: 'temp',
@@ -31,30 +31,11 @@ function chartTemp() {
                 borderColor: 'rgba(0, 255, 0, 1)',
                 borderWidth: 4,
                 lineTension: 0 // straight lines or curved
-            }, {
-                label: 'Dew Point °F',
-                yAxisID: 'temp',
-                data: arguments[3],
-                fill: false,
-                pointRadius: 2,
-                backgroundColor: 'rgba(0, 0, 255, 1)',
-                borderColor: 'rgba(0, 0, 255, 1)',
-                borderWidth: 4,
-                lineTension: 0 // straight lines or curved
-            }, {
-                label: 'Relative Humidity %',
-                yAxisID: 'RH',
-                data: arguments[4],
-                fill: false,
-                pointRadius: 2,
-                backgroundColor: 'rgba(0, 128, 128, 1)',
-                borderColor: 'rgba(0, 128, 128, 1)',
-                borderWidth: 4,
-                lineTension: 0 // straight lines or curved
             }]
         },
         options: {
-            aspectRatio: 1,
+            // aspectRatio: 1,
+            responsive: true,
             options: {
                 tooltips: {
                     mode: 'nearest'
@@ -73,7 +54,219 @@ function chartTemp() {
                         // min: Math.max.apply(Math, y_data) - (Math.min.apply(Math, y_data) * 0.25),
                         fontFamily: 'Manrope',
                         // maxTicksLimit: 5,
-                        beginAtZero: false
+                        beginAtZero: false,
+                        fontSize: 8
+                    },
+                    display: 'auto',
+                    scaleLabel: {
+                        display: true,
+                        labelString: '°F',
+                        fontFamily: 'Manrope'
+                    }
+
+                }],
+                xAxes: [{
+                    display: 'auto',
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Time',
+                        fontFamily: 'Manrope',
+                    },
+                    ticks: {
+                        fontFamily: 'Manrope',
+                        fontSize: 8
+                    }
+                }]
+            },
+            legend: {
+                display: true,
+                fontFamily: 'Manrope',
+                position: 'bottom',
+                labels: {
+                    boxWidth: 10,
+                    fontSize: 8,
+                }
+            }
+        }
+    });
+}
+
+// build the hourly rain/pressure chart
+function chartRain() {
+    const x_data = arguments[0];
+    const ctx = document.getElementById('rain');
+    const rainChart = new Chart(ctx, {
+        type: 'line',
+        defaults: {
+            global: {
+                defaultFontFamily: 'Manrope'
+            }
+        },
+        data: {
+            labels: x_data,
+            datasets: [{
+                label: 'Rain inches',
+                yAxisID: 'rain',
+                data: arguments[1],
+                fill: false,
+                pointRadius: 2,
+                backgroundColor: 'rgba(128, 64, 0, 1)',
+                borderColor: 'rgba(128, 64, 0, 1)',
+                borderWidth: 4,
+                lineTension: 0 // straight lines or curved
+            }, {
+                label: 'Barometric Pressure mb',
+                yAxisID: 'press',
+                data: arguments[2],
+                fill: false,
+                pointRadius: 2,
+                backgroundColor: 'rgba(64, 128, 0, 1)',
+                borderColor: 'rgba(64, 128, 0, 1)',
+                borderWidth: 4,
+                lineTension: 0 // straight lines or curved
+            }]
+        },
+        options: {
+            // aspectRatio: 1,
+            responsive: true,
+            options: {
+                tooltips: {
+                    mode: 'nearest'
+                }
+            },
+            scales: {
+                yAxes: [{
+                    id: 'rain',
+                    position: 'left',
+                    ticks: {
+                        // Include an inch symbol in the ticks
+                        callback: function (value, index, values) {
+                            return value + '"';
+                        },
+                        // max: (Math.max.apply(Math, y_data) * 1.25),
+                        // min: Math.max.apply(Math, y_data) - (Math.min.apply(Math, y_data) * 0.25),
+                        fontFamily: 'Manrope',
+                        // maxTicksLimit: 5,
+                        beginAtZero: false,
+                        fontSize: 8
+                    },
+                    gridLines: {
+                        drawOnChartArea: false,
+                    },
+                    display: 'auto',
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Rain',
+                        fontFamily: 'Manrope'
+                    }
+
+                }, {
+                    id: 'press',
+                    position: 'right',
+                    ticks: {
+                        // Include a degree symbol in the ticks
+                        callback: function (value, index, values) {
+                            return value + ' mb';
+                        },
+                        fontFamily: 'Manrope',
+                        // maxTicksLimit: 5,
+                        beginAtZero: false,
+                        fontSize: 8
+                    },
+                    gridLines: {
+                        drawOnChartArea: true,
+                    },
+                    display: 'auto',
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Barometric Pressure',
+                        fontFamily: 'Manrope',
+                    }
+                }],
+                xAxes: [{
+                    display: 'auto',
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Time',
+                        fontFamily: 'Manrope',
+                    },
+                    ticks: {
+                        fontFamily: 'Manrope',
+                        fontSize: 8
+                    }
+                }]
+            },
+            legend: {
+                display: true,
+                fontFamily: 'Manrope',
+                position: 'bottom',
+                labels: {
+                    boxWidth: 10,
+                    fontSize: 8,
+                }
+            }
+        }
+    });
+}
+
+// build the hourly humidity/Dew Pointchart
+function chartHumid() {
+    const x_data = arguments[0];
+    const cty = document.getElementById('humid');
+    const humidChart = new Chart(cty, {
+        type: 'line',
+        defaults: {
+            global: {
+                defaultFontFamily: 'Manrope'
+            }
+        },
+        data: {
+            labels: x_data,
+            datasets: [{
+                label: 'Dew Point °F',
+                yAxisID: 'dew',
+                data: arguments[1],
+                fill: false,
+                pointRadius: 2,
+                backgroundColor: 'rgba(0, 0, 255, 1)',
+                borderColor: 'rgba(0, 0, 255, 1)',
+                borderWidth: 4,
+                lineTension: 0 // straight lines or curved
+            }, {
+                label: 'Relative Humidity %',
+                yAxisID: 'RH',
+                data: arguments[2],
+                fill: false,
+                pointRadius: 2,
+                backgroundColor: 'rgba(0, 128, 128, 1)',
+                borderColor: 'rgba(0, 128, 128, 1)',
+                borderWidth: 4,
+                lineTension: 0 // straight lines or curved
+            }]
+        },
+        options: {
+            // aspectRatio: 1,
+            responsive: true,
+            options: {
+                tooltips: {
+                    mode: 'nearest'
+                }
+            },
+            scales: {
+                yAxes: [{
+                    id: 'dew',
+                    position: 'left',
+                    ticks: {
+                        // Include a degree symbol in the ticks
+                        callback: function (value, index, values) {
+                            return value + '°';
+                        },
+                        // max: (Math.max.apply(Math, y_data) * 1.25),
+                        // min: Math.max.apply(Math, y_data) - (Math.min.apply(Math, y_data) * 0.25),
+                        fontFamily: 'Manrope',
+                        // maxTicksLimit: 5,
+                        beginAtZero: false,
+                        fontSize: 8
                     },
                     display: 'auto',
                     scaleLabel: {
@@ -92,7 +285,8 @@ function chartTemp() {
                         },
                         fontFamily: 'Manrope',
                         // maxTicksLimit: 5,
-                        beginAtZero: false
+                        beginAtZero: false,
+                        fontSize: 8
                     },
                     gridLines: {
                         drawOnChartArea: false,
