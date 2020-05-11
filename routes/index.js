40540ad7f74b04763a515d326e296a69f88afdb4
alanv73 <alanv73@gmail.com>
2020-05-11 16:10:46 -0400

    added about page added about route removed errant s module

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,37 @@
 const { Sequelize, Model, DataTypes } = require('sequelize');
 const express = require('express'),
     app = express(),
-    router = express.Router();
+    router = express.Router(),
+    sequelize = require('../models/sqlize'),
+    WxMeasurement = require('../models/wxmeasr');
 
 // Root Route - landing page
 router.get('/', (req, res) => {
     res.render('landing');
+});
+
+// About Route - landing page
+router.get('/about', (req, res) => {
+    let currentTemp;
+
+    WxMeasurement.findOne({
+        attributes: [
+            'ID', 'CREATED', 'AMBIENT_TEMPERATURE'
+        ],
+        order: [
+            ['ID', 'DESC']
+        ],
+        raw: true
+    }).then(tempRow => {
+        currentTemp = tempRow.AMBIENT_TEMPERATURE;
+
+        res.render('about', {
+            temp: currentTemp
+        });
+    }).catch(err => {
+        console.error('Error :\n', err.message);
+    });
+
 });
 
 module.exports = router;
